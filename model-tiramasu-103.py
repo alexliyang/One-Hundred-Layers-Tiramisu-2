df3bd233587f29aeb0db3af6bb69d4243a16e695
demons <837940593@qq.com>
2018-01-24 14:49:46 +0800

    generalize model-tiramasu-103.py

--- model-tiramasu-103.py
+++ model-tiramasu-103.py
@@ -8,7 +8,8 @@ from keras.layers.core import Layer, Dense, Dropout, Activation, Flatten, Reshap
 from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D, Cropping2D
 from keras.layers.normalization import BatchNormalization
 
-from keras.layers import Conv2D, Conv2DTranspose
+from keras.layers import Conv2D, Conv2DTranspose, Input, Concatenate
+from keras.models import Model
 
 from keras import backend as K
 
@@ -23,106 +24,151 @@ from keras.regularizers import l2
  
 class Tiramisu():
 
+    # data_format: A string, one of channels_last (default) or channels_first.
+    # channels_last corresponds to inputs with shape (batch, height, width, channels).
+    # channels_first corresponds to inputs with shape (batch, channels, height, width).
+    # It defaults to the image_data_format value found in your Keras config file at ~/.keras/keras.json.
+    # I recommend using default setting rather than using explicit declaration.
+
+    # axis=-1 <--> data_format='channels_first'
+    # axis=1 <-> data_format='channels_last'
+
+    def __init__(self, input_shape=(224, 224, 3),
+                 first_conv_filters=48, growth_rate=12, pools=5,
+                 block_layers=[4, 5, 7, 10, 12, 15, 12, 10, 7, 5, 4]):
+        if type(block_layers) == list:
+            assert(len(block_layers) == 2 * pools + 1)
+        elif type(block_layers) == int:
+            block_layers = [block_layers] * pools
+        else:
+            raise ValueError
+
+        self.create(first_conv_filters, input_shape, pools, block_layers, growth_rate)
+
+    """
+    keras v1 (https://faroit.github.io/keras-docs/1.2.2/layers/normalization/)
+    mode: integer, 0, 1 or 2.
+    0: feature-wise normalization. Each feature map in the input will be normalized separately. 
+    1: sample-wise normalization. This mode assumes a 2D input.
+    2: feature-wise normalization, like mode 0, but using per-batch statistics to normalize the data during both testing and training.
+
+    keras v2 (https://keras.io/layers/normalization/)
+    The BatchNormalization layer no longer supports the mode argument.
+    """
+    def DenseBlock(self, filters):
+        def helper(input):
+            output = BatchNormalization(axis=1,
+                                        gamma_regularizer=l2(0.0001),
+                                        beta_regularizer=l2(0.0001))(input)
+            output = Activation('relu')(output)
+            output = Conv2D(filters, kernel_size=(3, 3), padding='same',
+                                     kernel_initializer="he_uniform")(output)
+            return output
+
+        return helper
+
+    def TransitionDown(self, filters):
+        def helper(input):
+            output = BatchNormalization(axis=1,
+                                        gamma_regularizer=l2(0.0001),
+                                        beta_regularizer=l2(0.0001))(input)
+            output = Activation('relu')(output)
+            output = Conv2D(filters, kernel_size=(1, 1), padding='same',
+                                     kernel_initializer="he_uniform")(output)
+            output = Dropout(0.2)(output)
+            output = MaxPooling2D(pool_size=(2, 2),
+                                  strides=(2, 2))(output)
+            return output
+
+        return helper
+
+    def TransitionUp(self, filters, input_shape = None):
+        def helper(input):
+            output = Conv2DTranspose(filters,  kernel_size=(3, 3), strides=(2, 2),
+                                               padding='same',
+                                               # input_shape=input_shape,
+                                               kernel_initializer="he_uniform")(input)
+            return output
+
+        return helper
 
 
-    def __init__(self):
-        self.create()
+    def create(self, first_conv_filters, input_shape, pools, block_layers, growth_rate):
+        def f():
+            input = Input(shape=input_shape)
 
-    def DenseBlock(self, layers, filters):
-        model = self.model
-        for i in range(layers):
-            model.add(BatchNormalization(mode=0, axis=1,
-                                         gamma_regularizer=l2(0.0001),
-                                         beta_regularizer=l2(0.0001)))
-            model.add(Activation('relu'))
-            model.add(Conv2D(filters,   kernel_size=(3, 3), padding='same',
-                                        kernel_initializer="he_uniform",
-                                        data_format='channels_last'))
-            model.add(Dropout(0.2))
+            #####################
+            # First Convolution #
+            #####################
 
-    def TransitionDown(self,filters):
-        model = self.model
-        model.add(BatchNormalization(mode=0, axis=1,
-                                     gamma_regularizer=l2(0.0001),
-                                     beta_regularizer=l2(0.0001)))
-        model.add(Activation('relu'))
-        model.add(Conv2D(filters, kernel_size=(1, 1), padding='same',
-                                  kernel_initializer="he_uniform"))
-        model.add(Dropout(0.2))
-        model.add(MaxPooling2D( pool_size=(2, 2),
-                                strides=(2, 2),
-                                data_format='channels_last'))
+            # We perform a first convolution. All the features maps will be stored in the Tiramisu.
+            # first_conv_filters is 48 in the one hundred layers tiramisu.
+            tiramisu = Conv2D(first_conv_filters, kernel_size=(3, 3), padding='same', 
+                              input_shape=input_shape,
+                              kernel_initializer="he_uniform",
+                              kernel_regularizer = l2(0.0001))(input)
 
-    def TransitionUp(self,filters,input_shape,output_shape):
-        model = self.model
-        model.add(Conv2DTranspose(filters,  kernel_size=(3, 3), strides=(2, 2),
-                                            padding='same',
-                                            output_shape=output_shape,
-                                            input_shape=input_shape,
-                                            kernel_initializer="he_uniform",
-                                            data_format='channels_last'))
+            #####################
+            # Downsampling path #
+            #####################
 
+            # The number of feature maps in the tiramisu is stored in the variable filters
+            filters = first_conv_filters
 
-    def create(self):
-        model = self.model = models.Sequential()
-        # cropping
-        # model.add(Cropping2D(cropping=((68, 68), (128, 128)), input_shape=(3, 360,480)))
+            skip_connection = []
 
-        model.add(Conv2D(48, kernel_size=(3, 3), padding='same', 
-                             input_shape=(224,224,3),
-                            kernel_initializer="he_uniform",
-                            kernel_regularizer = l2(0.0001),
-                            data_format='channels_last'))
+            for i in range(pools):
+                for j in range(block_layers[i]):
+                    l = self.DenseBlock(growth_rate)(tiramisu)
+                    tiramisu = Concatenate()([tiramisu, l])
+                    filters += growth_rate
+                skip_connection.append(tiramisu)
+                # You can't connect the new tiramisu and the old tiramisu, because their size are different.
+                tiramisu = self.TransitionDown(filters)(tiramisu)
 
-        # (5 * 4)* 2 + 5 + 5 + 1 + 1 +1
-        # growth_m = 4 * 12
-        # previous_m = 48
+            # reverse skip_connection
+            skip_connection = skip_connection[::-1]
 
-        self.DenseBlock(4,112) # 4*16 = 64 + 48 = 112
-        self.TransitionDown(112)
-        self.DenseBlock(5,192) # 5*16 = 80 + 112 = 192
-        self.TransitionDown(192)
-        self.DenseBlock(7,304) # 7 * 16 = 112 + 192 = 304
-        self.TransitionDown(304)
-        self.DenseBlock(10,464)
-        self.TransitionDown(464)
-        self.DenseBlock(12,656)
-        self.TransitionDown(656)
+            #####################
+            #     Bottleneck    #
+            #####################
 
-        self.DenseBlock(15,896) # m = 656 + 15x16 = 896
+            # We store now the output of the next dense block in a list.
+            # We will only upsample these new feature maps.
+            upsample_tiramisu = []
+            
+            for i in range(block_layers[pools]):
+                l = self.DenseBlock(growth_rate)(tiramisu)
+                upsample_tiramisu.append(l)
+                tiramisu = Concatenate()([tiramisu, l])
 
-        # upsampling part, m[B] is the sum of 3 terms
-        # 1. the m value corresponding to same resolution in the downsampling part (skip connection)
-        # 2. the number of feature maps from the upsampled block (n_layers[B-1] * growth_rate)
-        # 3. the number of feature maps in the new block (n_layers[B] * growth_rate)
-        #
-        self.TransitionUp(1088, (1088, 7, 7), (None, 1088, 14, 14))  # m = 656 + 15x16 + 12x16 = 1088.
-        self.DenseBlock(12,1088)
+            #######################
+            #   Upsampling path   #
+            #######################
 
-        self.TransitionUp(816, (816, 14, 14), (None, 816, 28, 28)) #m = 464 + 12x16 + 10x16 = 816
-        self.DenseBlock(10,816)
+            for i in range(pools):
+                tiramisu = self.TransitionUp(growth_rate * block_layers[i + pools])(tiramisu)
 
-        self.TransitionUp(576, (576, 28, 28), (None, 576, 56, 56)) # m = 304 + 10x16 + 7x16 = 576
-        self.DenseBlock(7,576)
+                upsample_tiramisu = []
+                for j in range(block_layers[i + pools]):
+                    l = self.DenseBlock(growth_rate)(tiramisu)
+                    upsample_tiramisu.append(l)
+                    tiramisu = Concatenate()([tiramisu, l])
 
-        self.TransitionUp(384, (384, 56, 56), (None, 384, 112, 112)) # m = 192 + 7x16 + 5x16 = 384
-        self.DenseBlock(5,384)
+            tiramisu = Conv2D(12, kernel_size=(1, 1),
+                                  padding='same',
+                                  kernel_initializer='he_uniform',
+                                  kernel_regularizer=l2(0.0001))(tiramisu)
+            tiramisu = Reshape((12, 224 * 224))(tiramisu)
+            tiramisu = Activation('softmax')(tiramisu)
 
-        self.TransitionUp(256, (256, 112, 112), (None, 256, 224, 224)) # m = 112 + 5x16 + 4x16 = 256
-        self.DenseBlock(4,256)
+            model = Model(inputs=input, outputs=tiramisu)
+            return model
 
-        model.add(Conv2D(12, kernel_size=(1,1), 
-                             padding='same',
-                             kernel_initializer="he_uniform",
-                             kernel_regularizer = l2(0.0001),
-                            data_format='channels_last'))
-        
-        model.add(Reshape((12, 224 * 224)))
-        model.add(Permute((2, 1)))
-        model.add(Activation('softmax'))
-        model.summary()
+        self.model = f()
+        self.model.summary()
 
         with open('tiramisu_fc_dense103_model.json', 'w') as outfile:
-            outfile.write(json.dumps(json.loads(model.to_json()), indent=3))
+            outfile.write(json.dumps(json.loads(self.model.to_json()), indent=3))
 
 Tiramisu()
