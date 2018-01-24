be5d2080dc57f3838ee6ac97cc065bf4505d8faf
demons <837940593@qq.com>
2018-01-24 14:57:21 +0800

    update bash

--- bash/train.sh
+++ bash/train.sh
@@ -4,6 +4,7 @@ source activate ImageSegmentation
 sudo apt-get update
 sudo apt-get install -y git
 cd ~ && git clone https://github.com/Clcanny/One-Hundred-Layers-Tiramisu.git
+cp -r ~/CamVid ~/One-Hundred-Layers-Tiramisu
 cd ~/One-Hundred-Layers-Tiramisu &&\
     python camvid_data_loader.py &&\
     python model-tiramasu-103.py &&\
