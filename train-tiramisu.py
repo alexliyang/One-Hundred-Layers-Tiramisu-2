ac9bcceb455ac374080d95bab83e12f5df2f2fa4
demons <837940593@qq.com>
2018-01-27 09:16:21 +0800

    .

--- train-tiramisu.py
+++ train-tiramisu.py
@@ -145,9 +145,10 @@ class CyclicLR(Callback):
         cycle = np.floor(1 + self.clr_iterations / (2 * self.step_size))
         x = np.abs(self.clr_iterations / self.step_size - 2 * cycle + 1)
         if self.scale_mode == 'cycle':
-            return self.base_lr + (self.max_lr-self.base_lr) * np.maximum(0, (1 - x)) * self.scale_fn(cycle)
+            print(self.base_lr + (self.max_lr - self.base_lr) * np.maximum(0, (1 - x)) * self.scale_fn(cycle))
+            return self.base_lr + (self.max_lr - self.base_lr) * np.maximum(0, (1 - x)) * self.scale_fn(cycle)
         else:
-            return self.base_lr + (self.max_lr-self.base_lr) * np.maximum(0, (1-x)) * self.scale_fn(self.clr_iterations)
+            return self.base_lr + (self.max_lr - self.base_lr) * np.maximum(0, (1 - x)) * self.scale_fn(self.clr_iterations)
 
     def on_train_begin(self, logs={}):
         logs = logs or {}
