74c8efc6da6ff05feed3b4506b30aa0818e1e39f
demons <837940593@qq.com>
2018-02-08 14:15:35 +0800

    modify data-loader

--- camvid_data_loader.py
+++ camvid_data_loader.py
@@ -12,21 +12,47 @@ import os
 # https://github.com/alexgkendall/SegNet-Tutorial
 DataPath = './CamVid/'
 # data_shape = 360*480
-data_shape = 224*224
+data_shape = 224 * 224
 
 
 def load_data(mode):
     data = []
     label = []
     with open(DataPath + mode +'.txt') as f:
+        # 示例数据
+        # /SegNet/CamVid/train/0001TP_006690.png /SegNet/CamVid/trainannot/0001TP_006690.png
         txt = f.readlines()
         txt = [line.split(' ') for line in txt]
     for i in range(len(txt)):
-        # data.append(np.rollaxis(normalized(cv2.imread(os.getcwd() + txt[i][0][7:])),2))
         # this load cropped images
-        data.append(np.rollaxis(normalized(cv2.imread(os.getcwd() + txt[i][0][7:])[136:,256:]),2))
-        label.append(one_hot_it(cv2.imread(os.getcwd() + txt[i][1][7:][:-1])[136:,256:][:,:,0],224,224))
-        print('.',end='')
+
+        # 关于numpy.rollaxis()的说明
+        # axes = list(range(0, n))
+        # axes.remove(axis)
+        # axes.insert(start, axis)
+        # return a.transpose(axes)
+        # 把元素从一个位置移动到另一个位置
+
+        # 关于os.getcwd()的说明
+        # os.getcwd()
+        # '/Users/demons/Documents/One-Hundred-Layers-Tiramisu'
+
+        # txt[i][0][7:]的示例数据
+        # /CamVid/train/0001TP_006690.png
+
+        # image.shape = (360, 480, 3)
+        # 360 - 136 = 224
+        # 480 - 256 = 224
+        # ...[136:, 256:]通过裁剪得到右下角的图片
+
+        # arr = np.ones([224, 224, 3])
+        # np.rollaxis(arr, 2).shape
+        # (3, 224, 224)
+        # 如果我们使用tensorflow作为backend，可能不需要改变图像的维度顺序
+
+        data.append(np.rollaxis(normalized(cv2.imread(os.getcwd() + txt[i][0][7:])[136:, 256:]), 0))
+        label.append(one_hot_it(cv2.imread(os.getcwd() + txt[i][1][7:][:-1])[136:, 256:][:, :, 0], 224, 224))
+        print('.', end='')
     return np.array(data), np.array(label)
 
 
