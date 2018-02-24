2680e4c6bd504d017501b2569ed53f7fa3cab0a5
demons <837940593@qq.com>
2018-02-24 16:20:34 +0800

    meet a very strange problem

--- superpixel.py
+++ superpixel.py
@@ -1,8 +1,13 @@
 # import the necessary packages
 from skimage.segmentation import slic, mark_boundaries
 from skimage.io import imread, imsave
+from skimage import img_as_ubyte, img_as_float
+import numpy as np
 
 image = imread('test.png')
 segments = slic(image, n_segments=300)
-image = mark_boundaries(image, segments)
+image = mark_boundaries(np.zeros(image.shape), segments)
+for x in image[:, :, 0]:
+    print(x)
+    break
 imsave('superpixel.png', image)
