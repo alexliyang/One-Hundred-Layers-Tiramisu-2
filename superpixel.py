ba693eb895d3fa25affa9fa24b65dd3291f82594
demons <837940593@qq.com>
2018-02-24 17:18:10 +0800

    get three channel from three different superpixels

--- superpixel.py
+++ superpixel.py
@@ -5,9 +5,12 @@ from skimage import img_as_ubyte, img_as_float
 import numpy as np
 
 image = imread('test.png')
+segments = slic(image, n_segments=100)
+channel1 = mark_boundaries(np.zeros(image.shape), segments, color=(1, 1, 1))[:, :, 0]
+segments = slic(image, n_segments=200)
+channel2 = mark_boundaries(np.zeros(image.shape), segments, color=(1, 1, 1))[:, :, 0]
 segments = slic(image, n_segments=300)
-image = mark_boundaries(np.zeros(image.shape), segments)
-for x in image[:, :, 0]:
-    print(x)
-    break
+channel3 = mark_boundaries(np.zeros(image.shape), segments, color=(1, 1, 1))[:, :, 0]
+image = np.dstack((channel1, channel2, channel3))
+
 imsave('superpixel.png', image)
