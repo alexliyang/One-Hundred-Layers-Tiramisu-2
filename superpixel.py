dd13038a77006f1a36cf939a1b047343fa11bc2b
demons <837940593@qq.com>
2018-02-24 12:58:57 +0800

    simplify superpixel.py

--- superpixel.py
+++ superpixel.py
@@ -1,27 +1,8 @@
 # import the necessary packages
-from skimage.segmentation import slic
-from skimage.segmentation import mark_boundaries
-from skimage.util import img_as_float
-from skimage import io
-import matplotlib.pyplot as plt
-import argparse
- 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True, help = "Path to the image")
-args = vars(ap.parse_args())
- 
-# load the image and convert it to a floating point data type
-image = img_as_float(io.imread(args["image"]))
- 
-# apply SLIC and extract (approximately) the supplied number of segments
-segments = slic(image, n_segments=300)
+from skimage.segmentation import slic, mark_boundaries
+from skimage.io import imread, imsave
 
-# show the output of SLIC
-fig = plt.figure("Superpixels")
-ax = fig.add_subplot(1, 1, 1)
-ax.imshow(mark_boundaries(image, segments))
-plt.axis("off")
- 
-# show the plots
-plt.show()
+image = imread('test.png')
+segments = slic(image, n_segments=300)
+image = mark_boundaries(image, segments)
+imsave('superpixel.png', image)
